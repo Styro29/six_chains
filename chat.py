85a0ae5265a10bd465fd833c93ee3d3fff527922
a-mink <ahmin.k95@gmail.com>
2024-04-17 12:15:50 +0000

    class구현 완료

--- chat.py
+++ chat.py
@@ -8,16 +8,20 @@ chat.py 구현할 기능
 '''
 
 import os
-import gemini
+import json
+from gemini import AI_Model
 from langchain_core.prompts import SystemMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.document_loaders import WebBaseLoader
 
+model = ChatGoogleGenerativeAI()
+
 # 대화 기록을 저장할 파일 경로
+CHAT_HISTORY_FILE = "chat_history.json"
 
 class Chat:
-    def __init__(self, chat_history_file = "chat_history.txt"):
+    def __init__(self, chat_history_file = CHAT_HISTORY_FILE):
         self.default_prompt = []
         self.user_input = ""
         self.chat_history_file = chat_history_file
@@ -25,10 +29,8 @@ class Chat:
         
     def load_chat_history(self):
         # 대화 기록 파일이 있는 경우, 파일에서 읽어옴
-        if os.path.exists(self.chat_history_file): # type: ignore
-            with open(self.chat_history_file, 'r') as file:
-                chat_history = file.readlines()
-            return chat_history
+        if os.path.exists(self.chat_history_file): 
+            return json.read_json(self.chat_history_file)
         else:
             return []
 
@@ -37,33 +39,25 @@ class Chat:
         with open(self.chat_history_file, 'a') as file:
             file.writelines(str({'user' : self.user_input, 'chatbot' : model.get_response(default_prompt, user_input)}))
 
+    def main(model, retriever):
 
-def main(model, retriever):
-    C = Chat()
+        print("Welcome to 커리어 상담봇!")
 
-    print("Welcome to ChatBot!")
+        while True:
+            # 사용자 질문 입력 받기
+            user_input = input("데이터 직무에 대해 궁금한 점을 말씀해주세요!")
 
-    while True:
-        # 사용자 질문 입력 받기
-        user_input = input("이어드림 스쿨에 대해 궁금한 점을 입력하세요!")
+            # 사용자가 종료하고자 할 때까지 반복
+            if user_input.lower() == 'exit':
+                print("대화를 종료합니다.")
+                break
 
-        # 사용자가 종료하고자 할 때까지 반복
-        if user_input.lower() == 'exit':
-            print("대화를 종료합니다.")
-            break
+            # 프롬포트 설정
+            default_prompt = ("system", "At first, Answer only syntax. And then add explanation.")
 
-        # 프롬포트 설정
-        default_prompt = ("system", "At first, Answer only syntax. And then add explanation.")
+            response = model.get_response(user_input)
+            Chat.save_chat_history(user_input, response)
 
-        model.get_response(default_prompt, user_input)
-        retriever()
-
-
-        # 대화 기록에 추가
-        
-
-        # 대화 기록 저장
-        C.save_chat_history(last_qna)
 
 if __name__ == "__main__":
-    main()
+    Chat.main()
