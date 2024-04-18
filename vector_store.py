c89cfa9c44bc6988518ffe3a0bb0a5152189bbcf
jwhan <jwhan190124@gmail.com>
2024-04-18 04:44:11 +0000

    final commit

--- vector_store.py
+++ vector_store.py
@@ -1,20 +1,42 @@
-import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.prompts import SystemMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import FAISS
 
 
-# 만약 컴퓨터에 GOOGLE_API_KEY가 저장되어있지 않다면 새로 저장하라는 코드
-if "GOOGLE_API_KEY" not in os.environ:
-    os.environ["GOOGLE_API_KEY"] = ""
+class VectorStore:
 
-loader = WebBaseLoader("")
-text = loader.load()
-print(text)
+    def __init__(self):
+        pass
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=, chunk_overlap=)
-splits = text_splitter.split_documents(text)
-print(splits)
+
+    def text_split(self, data):
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500,
+            chunk_overlap=100,
+            length_function=len
+        )
+        splits = text_splitter.split_documents(data)
+        return splits
+
+    def embed_documents(self, splits):
+        embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        self.vectorstore = FAISS.from_documents(documents=splits, embedding=embedding_model)
+        return self.vectorstore
+\
+
+    def store(self, splits):
+        splits = self.text_split(source)
+        self.vectorstore = self.embed_documents(splits)
+        return self.vectorstore
+
+
+# # Example usage
+
+# crawl = Crawler
+# source = crawl.get_data_from_url()
+# vector_store = VectorStore()
+# vectordb = vector_store.store(source)
+# print(vectordb.index_to_docstore_id)
