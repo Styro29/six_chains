be93322f81f3796166ea25fb0f774d13ce7c04cd
wooyeol.park <twinis@gmail.com>
2024-04-19 06:56:17 +0000

    retrieve.py mmr 검색 방식 적용 및 README.md 파일 수정

--- retrieve.py
+++ retrieve.py
@@ -3,5 +3,5 @@ class Retriever:
         pass
 
     def retrieve(self, vec_db, result_num):     # 필요한 변수: vec_db -> vectorDB, result_nim -> retrieve 출력 개수
-        retriever = vec_db.as_retriever(k=result_num)        # VectorDB를 입력받아 retriever 인스턴스 생성
+        retriever = vec_db.as_retriever(search_type="mmr", search_kwargs={'k': result_num, 'fetch_k': 50})        # VectorDB를 입력받아 retriever 인스턴스 생성
         return retriever                                     # 생성된 retriever 인스턴스 반환
